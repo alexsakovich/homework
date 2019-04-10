dc0e16c0e1350005fdd943e336c4bc66698aabd9
Alex Sakovich <a.sakovich90@gmail.com>
2019-04-10 17:25:03 +0300

    Good to go

--- src/LessonFourAnimals/Animals.java
+++ src/LessonFourAnimals/Animals.java
@@ -1,12 +1,32 @@
 package LessonFourAnimals;
 
+import java.util.Scanner;
+
 public class Animals {
-    public static void main(String[] args) {
-        Dog myDog1 = new Dog(15, "черный");
-        myDog1.myDog();
-        Dog myDog2 = new Dog("Собака", 8);
-        myDog2.myDog();
-        Dog myDog3 = new Dog("Собака 2", 25, 8);
-        myDog3.myDog();
+    public static void main(String[] args)
+            throws java.io.IOException {
+
+        Scanner in = new Scanner(System.in);
+        char choice;
+        System.out.println("1. Собаки");
+        System.out.println("2. Кошки");
+        choice = (char) System.in.read();
+
+        switch (choice) {
+            case '1':
+                Dog myDog1 = new Dog(15, "черный");
+                myDog1.myDog();
+                Dog myDog2 = new Dog("Собака", 8);
+                myDog2.myDog();
+                break;
+            case '2':
+                Cat myCat1 = new Cat("cat1", 1, 2, 3);
+                Cat myCat2 = new Cat("cat2", 4, 5, 6);
+                System.out.println(myCat1.pawFight(myCat2));
+                System.out.println(myCat2.pawFight(myCat1));
+                break;
+            default:
+                System.out.println("Животное не выбрано");
+        }
     }
 }
