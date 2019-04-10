9e4a62544d8861b6703eebef9d231ccd7bf0e21a
Alex Sakovich <a.sakovich90@gmail.com>
2019-04-10 12:45:00 +0300

    Work in progress

--- src/LessonFourAnimals/Animals.java
+++ src/LessonFourAnimals/Animals.java
@@ -2,7 +2,11 @@ package LessonFourAnimals;
 
 public class Animals {
     public static void main(String[] args) {
-        Dog dog = new Dog ();
-        System.out.println(dog);
+        Dog myDog1 = new Dog(15, "черный");
+        myDog1.myDog();
+        Dog myDog2 = new Dog("Собака", 8);
+        myDog2.myDog();
+        Dog myDog3 = new Dog("Собака 2", 25, 8);
+        myDog3.myDog();
     }
 }
